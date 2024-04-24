7fb7bb1756095bca64bb2a21ea0f8f65266f7f16
Victor Escareno <victorem.inbox@gmail.com>
2024-04-24 11:36:27 -0600

    modificacion minima en estructura

--- 02-grafica.R
+++ 02-grafica.R
@@ -28,12 +28,25 @@ ggsave( filename = "results/barras.png",
 # que pierden el SNI
 
 # Leer la data
-resumen <- vroom( file = "results/resumen.csv")
+resumen <- read.csv( file = "results/resumen.csv")
 pay.p <- ggplot( data = resumen,
                  mapping = aes( x = "",
                                 y = totales,
-                                fill = nobilis))
+                                fill = nobilis))+
   geom_col( color = "black") +
     coord_polar("y") +
     theme_void() + 
     ggtitle( "Miembros que han perdido el SNI")
+
+# Visualizarlo
+pay.p
+
+# Guardar el grafico
+ggsave( filename = "results/pay.png", bg = "white",
+        plot = pay.p , width = 7, height = 7)
+
+#TO DOs
+#victor: Agregar el numero de Drs y Dras en el pay
+
+
+
