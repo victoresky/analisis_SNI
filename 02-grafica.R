ca5682c0ffad4b769c98dae24e146a95ae99995f
Victor Escareno <victorem.inbox@gmail.com>
2024-04-24 11:22:05 -0600

    Graficar cambios anuales

---
+++ 02-grafica.R
@@ -0,0 +1,39 @@
+# cargar paquetes
+pacman::p_load("ggplot2")
+
+# Graficar cambios por anio
+
+
+# Leer la data
+limpia <- read.csv(file = "results/limpia.csv")
+
+# Graficamos
+barras.p <- ggplot( data = limpia,
+                    mapping = aes( x = year,
+                                   y = miembros,
+                                   fill = cambio)) +
+  geom_col( position = "dodge") +
+  ggtitle("Cambiar en el SNI") +
+  theme_classic() +
+  facet_wrap(~nobilis)
+
+# Visualizamos 
+barras.p
+
+# Guardar el grafico
+ggsave( filename = "results/barras.png",
+        plot = barras.p, width = 7, height = 5)
+
+# Graficar la proporcion de DR y DRA
+# que pierden el SNI
+
+# Leer la data
+resumen <- vroom( file = "results/resumen.csv")
+pay.p <- ggplot( data = resumen,
+                 mapping = aes( x = "",
+                                y = totales,
+                                fill = nobilis))
+  geom_col( color = "black") +
+    coord_polar("y") +
+    theme_void() + 
+    ggtitle( "Miembros que han perdido el SNI")
